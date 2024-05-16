226181f3c91b77bb1f9860f28f716e5c051901ed
Aleksandar Dobrinic <aleksandar.dobrinic@predavaci.algebra.hr>
2024-05-16 18:37:47 +0200

    Intor to PHP

---
+++ 2.UvodPHP/php_i_html.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP & HTML</title>
+</head>
+<body>
+    <h1>PHP & HTML</h1>
+    
+    <?php
+        $ispis = "Ovo je ispisano pomocu PHP jezika."
+    ?>
+
+    <section>
+        <p><?php echo $ispis; ?></p>
+    </section>
+</body>
+</html>
