a0822525ad32503784be64a8c2eb9247ba2ff03a
Aleksandar Dobrinic <aleksandar.dobrinic@predavaci.algebra.hr>
2024-05-23 21:32:38 +0200

    dan 6

---
+++ 5.kontrolne_strukture/zadaca.php
@@ -0,0 +1,25 @@
+1. Koristeći petlju while, ispišite prvih deset brojeva.
+
+2. Koristeći petlju for, ispišite visekratnike broja 3 do 100.
+
+3. Tablica množenja: Napiši PHP program koji koristi petlje za generiranje tablice množenja od 1 do 10.
+    Primjer:
+        1 x 1 = 1
+        1 x 2 = 2
+        ...
+        10 x 10 = 100
+
+4. Definirajte varijablu $names i dodijelite joj niz koji sadrži pet imena.
+    Koristeći petlju foreach, iz niza ispišite ključeve i pripadajuće im vrijednosti. 
+
+6. Ispisati imena iz niza $names spojene sa zarezom i razmakom tako da iza zadnjeg imena ne budu zarez i razmak
+    Primjer:
+        Harry, Ron, Hermione, Snake
+
+7. Definirajte varijable a, b i c, te im istim redoslijedom dodijelite vrijednosti 5,10 i 15.
+    Koristeći uvjetovani tip kontrolne strukture provjerite je li vrijednost b između a i c.
+    Ako je uvjet istinit, ispišite da je b između a i c, a ako je uvjet lažan ispišite da nije.
+    Kod mora raditi i ako zamijenimo vrijednosti u varijablama a i c. 
+
+8. Koristeći uvjetovani tip kontrolne strukture switch ili match ispišite koji je trenutno dan u tjednu.
+    Za ispravno izvršenu vježbu koristite PHP funkciju date(). Nazivi dana moraju biti na hrvatskom jeziku.
